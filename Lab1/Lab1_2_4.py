d758ebc9a2bf08bd8d247e9566a0670df5c85719
annGordova <113303513+annGordova@users.noreply.github.com>
2024-02-09 13:21:35 +0300

    Задания 2-4 целиком

--- Lab1/Lab1_2_4.py
+++ Lab1/Lab1_2_4.py
@@ -1,5 +1,34 @@
+class No_num(Exception):
+    pass
 
+def f4(s):
+    if s == s[::-1]:
+        print('Строка палиндром')
+    else:
+        print('Строка не палиндром')
+
+def f11(s):
+    a = s.split()
+    print('Число слов ' + str(len(a)))
 
 def f15(x):
     print('Число различных символов ' + str(len(set(str(x)))))
 
+print('Введите номер задачи: 4, 11 или 15')
+num = int(input())
+try:
+    if num not in [4, 11, 15]:
+        raise No_num
+except No_num:
+    print('Ошибка')
+
+
+if num == 4:
+    si = input('Введите строку ')
+    f4(si)
+elif num == 11:
+    si = input('Введите строку ')
+    f11(si)
+elif num == 15:
+    xi = input('Введите число ')
+    f15(xi)
