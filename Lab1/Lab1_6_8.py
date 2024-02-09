ffaeefc110644b0f8df41a50a958fc09fd8d4d10
annGordova <113303513+annGordova@users.noreply.github.com>
2024-02-09 14:09:01 +0300

    Задание 8

--- Lab1/Lab1_6_8.py
+++ Lab1/Lab1_6_8.py
@@ -1,10 +1,16 @@
 
 
-def f15(s):
-    a = s.split()
-    k = 0
-    for si in a:
-        if int(si) > 5:
-            k += 1
-    return k
-
+def f11(s):
+    i1 = ord('a')
+    i1_e = ord('z')
+    i2 = ord('A')
+    i2_e = ord('Z')
+    a1 = [chr(i) for i in range(i1, i1_e+1)]
+    a2 = [chr(i) for i in range(i2, i2_e+1)]
+    for x in s:
+        if x in a1:
+            a1.remove(x)
+        if x in a2:
+            a2.remove(x)
+    print(*a1)
+    print(*a2)
