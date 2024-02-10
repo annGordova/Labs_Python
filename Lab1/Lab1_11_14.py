0d6a9aea53bb9698dd19359aed950542981019ec
annGordova <113303513+annGordova@users.noreply.github.com>
2024-02-10 12:08:30 +0300

    Задание 12

--- Lab1/Lab1_11_14.py
+++ Lab1/Lab1_11_14.py
@@ -2,13 +2,21 @@
 import numpy as np
 
 
-def f1(s):
-    gls = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"]
-    k_gl, k_sogl = 0, 0
-    for x in s:
-        if x in gls:
-            k_gl += 1
+def calculate_deviation_f5(string, average_frequencies):
+    char_counts = {}
+    for char in string:
+        if char in char_counts:
+            char_counts[char] += 1
         else:
-            k_sogl += 1
-    return abs(k_gl-k_sogl)
+            char_counts[char] = 1
+
+    max_char = max(char_counts, key=char_counts.get)
+    max_frequency = char_counts[max_char] / len(string)
+
+    deviation = (max_frequency - average_frequencies[max_char]) ** 2
+    return deviation
+
+
+def sort_strings_f5(strings, average_frequencies):
+    return sorted(strings, key=lambda x: calculate_deviation_f5(x, average_frequencies))
 
