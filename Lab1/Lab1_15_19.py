d56cfc27d17801ac171a845571d05349d58b4cad
annGordova <113303513+annGordova@users.noreply.github.com>
2024-02-10 12:15:44 +0300

    Задание 19

--- Lab1/Lab1_15_19.py
+++ Lab1/Lab1_15_19.py
@@ -1,8 +1,18 @@
 
 
-def f40(a):
-    return min(x for x in a if x%2==0)
+def Simple_f52(x):
+    for i in range(2, int(x**0.5)+1):
+        if x%i==0:
+            return False
+    return True
 
-
-
-
+def f52(x):
+    a = []
+    while not(Simple_f52(x)):
+        for i in range(2, x):
+            if x%i==0:
+                a.append(i)
+                x//=i
+                break
+    a.append(x)
+    return a
